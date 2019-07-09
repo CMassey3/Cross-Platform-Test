0c3b9d80e28b2fe9bdc4db0ed1b5106ea81c2742
CMassey3 <47466818+CMassey3@users.noreply.github.com>
2019-07-09 19:27:06 -0400

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var firstName = "Camden";
+            var lastName = "Massey";
+
+            var fullName = firstName + " " + lastName;
+            var myFullName = string.Format("My Name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(", ", names);
+            Console.WriteLine(formattedNames);
+
+            var me = new string[2] { firstName, lastName };
+            var personalName = string.Join(" ", me);
+            Console.WriteLine(personalName);
+
+            var text = @"Hi John
+     Look into the following paths
+     c:\folder1\folder2
+     c:\folder3\folder4";
+
+            Console.WriteLine(text);
+        }
+    }
+}
